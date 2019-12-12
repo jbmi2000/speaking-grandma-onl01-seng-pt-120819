bb472953ad159751a1956ad9ca567d90fa094d84
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-12 03:11:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
-require 'pry'
+
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    binding.pry
   else
     return "NO, NOT SINCE 1938!"
   end
